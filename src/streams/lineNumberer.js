29aba5e6c784f00783c5ab719d1487b36494496b
MaryAnzh <maryang@mail.ru>
2026-03-08 21:23:42 +0300

    feet(stream): add lineNumberer func

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,34 @@
+import { Transform } from 'stream';
+
+// test
+// node src\streams\lineNumberer.js
+// name\nis
 const lineNumberer = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Use Transform Stream to prepend line numbers
-  // Write to process.stdout
+  let lineCount = 1;
+
+  const transformer = new Transform({
+    readableObjectMode: false,
+    writableObjectMode: false,
+
+    transform(chunk, encoding, callback) {
+      const text = chunk.toString();
+
+      // Windows fix: remove \r
+      const cleaned = text.replace(/\r/g, '');
+      let lines = cleaned
+        .split('\n')
+        .flatMap(l => l.split('\\n'))
+        .filter(Boolean);
+
+      const setLine = lines
+        .map(content => `${lineCount++} | ${content}`)
+        .join('\n');
+
+      callback(null, setLine);
+    }
+  });
+
+  process.stdin.pipe(transformer).pipe(process.stdout);
 };
 
 lineNumberer();
